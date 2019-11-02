73aba2835a2d49b1d92b26da6a644449145cf78f
shubhamsingh <shubhamsingh>
2019-11-02 22:05:23 +0530

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('LibraryManagementSystem app is running!');
+    expect(page.getTitleText()).toEqual('librarymanagementsystem-angular app is running!');
   });
 
   afterEach(async () => {
